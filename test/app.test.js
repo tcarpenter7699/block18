9592ce81fcc6a8f399e3118f113683926665bb65
Trent Carpenter <tcarpenter7699@gmail.com>
2024-07-18 10:21:12 -0500

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,22 +1,22 @@
 const {logIn, createAccount, signUpWithGoogle} = require("../app.js");
 
 test('was logging in successful?', ()=>{
-    expect(logIn("maya", "")).toBeFalsy();
+    expect(logIn("trent", "")).toBeFalsy();
     expect(logIn("", "pass")).toBeFalsy();
     expect(logIn("", "")).toBeFalsy();
-    expect(logIn("maya", "pass")).toBeTruthy();
+    expect(logIn("trent", "pass")).toBeTruthy();
 })
 
 test('was the create account successful?', ()=>{
-    expect(createAccount("mimmo888@exampleemail.com", "")).toBeFalsy();
+    expect(createAccount("tcarpenter7699@exampleemail.com", "")).toBeFalsy();
     expect(createAccount("", "pass")).toBeFalsy();
     expect(createAccount("", "")).toBeFalsy();
-    expect(createAccount("mimmo888@exampleemail.com", "pass")).toBeTruthy();
+    expect(createAccount("tcarpenter7699@exampleemail.com", "pass")).toBeTruthy();
 })
 
 test('was the email verification successful?', ()=>{
-    expect(signUpWithGoogle("mimmo888@exampleemail.com", "")).toBeFalsy();
+    expect(signUpWithGoogle("tcarpenter7699@exampleemail.com", "")).toBeFalsy();
     expect(signUpWithGoogle("","1 2 3 4 5")).toBeFalsy();
     expect(signUpWithGoogle("", "")).toBeFalsy();
-    expect(signUpWithGoogle("mimmo888@exampleemail.com", "1 2 3 4 5")).toBeTruthy();
+    expect(signUpWithGoogle("tcarpenter7699@exampleemail.com", "1 2 3 4 5")).toBeTruthy();
 })
