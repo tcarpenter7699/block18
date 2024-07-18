d479366618897036571df4869ea2f4c121d5f03b
tcarpenter7699 <tcarpenter7699@gmail.com>
2024-07-18 10:16:57 -0500

    done

---
+++ app.js
@@ -0,0 +1,33 @@
+function logIn(username, password){
+
+  // if the user does not input a username OR password, the function should return false, otherwise should return true if both values have SOME information inside 
+  if(username.length===0 || password.length===0) {
+      return false
+  } else if (username==="maya" && password==="pass"){
+      return true 
+  } else {
+      return false
+  }
+}
+
+function createAccount(email, password){
+  if(email.length===0 || password.length===0) {
+      return false
+  } else if (email==="mimmo888@exampleemail.com" && password==="pass"){
+      return true
+  } else {
+      return false
+  }
+}
+
+function signUpWithGoogle(email, verificationCode){
+  if(email.length===0 || verificationCode===0) { 
+      return false
+  }else if(email==="mimmo888@exampleemail.com" && verificationCode==="1 2 3 4 5"){
+      return true
+  } else {
+      return false
+  }
+}
+
+module.exports = {logIn, createAccount, signUpWithGoogle}
